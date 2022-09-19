fedf6f0c12bffad98ae0d93ef1a84b9579de3aae
malaknaji1 <malak.naji1111@gmail.com>
2022-09-19 21:39:59 +0300

    the first js task

---
+++ app.js
@@ -0,0 +1,4 @@
+
+
+document.getElementById("screenSize").innerHTML="أبعاد شاشتك الحالية : " + screen.width + " عرض * " + screen.height + "  ارتفاع";
+document.getElementById("windowSize").innerHTML="أبعاد النافذة الحالية : " + window.innerWidth + " عرض * " + window.innerHeight + "  ارتفاع" ;
